5228564636ba6f39d8cf8d2efa021a494005bc09
sainadhmanyam <76801368+sainadhmanyam@users.noreply.github.com>
2024-02-19 19:26:46 -0500

    added the project to git repo

---
+++ src/main/java/com/springpractice/SpringBootMicroservices/HelloController.java
@@ -0,0 +1,4 @@
+package com.springpractice.SpringBootMicroservices;
+
+public class HelloController {
+}
---
+++ src/test/java/com/springpractice/SpringBootMicroservices/SpringBootMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.SpringBootMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
