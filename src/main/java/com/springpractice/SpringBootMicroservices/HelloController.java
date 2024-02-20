c93b049aa8c8e0eea212c5e0a432d37ec69d644d
sainadhmanyam <76801368+sainadhmanyam@users.noreply.github.com>
2024-02-20 06:07:50 -0500

    pushing the project to git repo

--- src/main/java/com/springpractice/SpringBootMicroservices/HelloController.java
+++ src/main/java/com/springpractice/SpringBootMicroservices/HelloController.java
@@ -1,4 +1,14 @@
 package com.springpractice.SpringBootMicroservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
 }
